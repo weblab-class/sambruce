2c7ca7e779f900ba6b40a1a848fd46867ab1c37c
Sam Bruce <sam@dhcp-10-29-118-72.dyn.mit.edu>
2022-01-16 22:09:44 -0500

    Working Connect Button

--- client/src/components/pages/Connect.js
+++ client/src/components/pages/Connect.js
@@ -8,23 +8,37 @@ import "./Connect.css";
 
 const Connect = ({userId}) => { 
     const [newUser,setNewUser] = useState({name:undefined});
-    const getRandomUser = () => { //Returns user data for random user
+    const [changeUser,setChangeUser] = useState(0);
+    
+    const getRandomUser = (update_user) => { //Returns user data for random user
         //Make API call to get random userId, not you or anyone youve messaged
-        get("/api/randuser",{id:userId}).then((data) => setNewUser(data));
-            
-        //Make API call to get that users data
+        get("/api/randuser",{id:userId, update:update_user}).then((data) => setNewUser(data));
+        return({});
     }
 
-    const updateConnections = (matchId) => {
+    /* const updateConnections = () => {
         //API call that updates the list of matches
-    }
+        console.log(newUser.name);
+        console.log(newUser.userId);
+        if(newUser && newUser.name) {
+          post("/api/addconnection",[userId,newUser.userId]);
+          console.log("adding user");
+        }
+    } */
+    useEffect(() => {
+      if(userId){
+        getRandomUser(false);
+      }
+    },[changeUser]);
 
-    useEffect(() => getRandomUser(),[userId]);
+    useEffect((() => setChangeUser(changeUser+1)),[userId]);
 
   return (
     <div className="Connect-container">
         {newUser.name?
-        (<div> {newUser.name}</div>)
+        (<div> {newUser.name}
+          <button onClick={() => getRandomUser(true)} /> 
+        </div>)
         : (<div> Login to Access Content </div>)
         }
     </div>
--- server/api.js
+++ server/api.js
@@ -12,6 +12,7 @@ const express = require("express");
 // import models so we can interact with the database
 const User = require("./models/user");
 const Profile = require("./models/Profilemodel");
+const ConnectList = require("./models/ConnectList");
 
 // import authentication library
 const auth = require("./auth");
@@ -46,8 +47,47 @@ router.get("/randuser",(req,res) => {
     return res.send({});
   }
   let query = {userId : {$ne : req.query.id}}
-  Profile.findOne(query).then((profile) => res.send(profile));
+  Profile.findOne(query).then((profile) => {
+    ConnectList.findOne({userId:req.query.id}).then((cList) => {
+      if (req.query.update == "true") {
+        if (cList && cList.userId) {
+          console.log("found profile");
+          console.log("connectId: ".concat(profile.userId));
+          console.log(cList);
+          cList.connections = [...cList.connections,profile.userId];
+          console.log(cList);
+          cList.save();
+        } 
+        else {
+          console.log(cList);
+          newConnectProfile = new ConnectList({userId:req.query.id, connections:[profile.userId]});
+          newConnectProfile.save();
+      }
+    }
+  });
+    
+    res.send(profile);
+  }
+);
 });
+
+/* router.post("/addconnection", (req,res) => {
+  [user_id,connectId] = req.body;
+  ConnectList.findOne({userId:user_id}).then((cList) => {
+    
+    if (cList && cList.userId) {
+      console.log("found profile");
+      console.log("connectId: ".concat(connectId));
+      cList.connections = [...cList.connections,connectId];
+    }
+    else {
+      console.log(cList);
+      newConnectProfile = new ConnectList({userId:user_id, connections:[connectId]});
+      newConnectProfile.save();
+    }
+  })
+  res.send({});
+}); */
 
 router.post("/updateuserdata",(req,res) => {
   [username,loc,sch,fav,user_id] = req.body
@@ -62,6 +102,7 @@ router.post("/updateuserdata",(req,res) => {
   })
   profile.save();
   });
+  res.send({});
 });
 
 router.post("/initsocket", (req, res) => {
